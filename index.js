552910fca6773ef80eb38aa382a1e115b5ec2292
Madhuresh Gupta <madhuresh97@yahoo.co.in>
2020-12-11 22:50:10 +0530

    added resolvers and schema

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Start your es6 scripts here
+import express from 'express'
+import resolvers from './resolvers.js'
+import schema from './schema.js'
+
+import { graphqlHTTP } from 'express-graphql'
+
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Up and running with graphQL")
+})
+
+const root = { sampleValue: () => console.log("Madhuresh Gupta graphQL") }
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
+
+app.listen(8082, () => console.log("Running at 8082"))
